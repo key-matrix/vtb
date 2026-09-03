1aacd4df4419cdfb74983cc0d90164fb1c21405e
KyoheiOgawa <kyoheiogawa10@gmail.com>
2026-09-03 15:19:03 +0900

    修正2

--- confrict_test/sample1.dart
+++ confrict_test/sample1.dart
@@ -6,6 +6,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 
+
+
+// TODO1 : dev/master ブランチをcloneし、自分の作業ブランチを分岐させてください。
+//          命名規則は「dev/feature/あなたの名前」 ロアスネークケースです。
+// TODO2 : このファイルの内容を対象の行にコメントを各々追記してください、
+// TODO3 : プッシュして、他のメンバーが作成したブランチとマージ作業を行います。[※説明があるまで進めないでください]
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -26,11 +33,6 @@ void main() async {
     ),
   );
 }
-
-// TODO1 : dev/master ブランチから分岐させたブランチを作成してください、命名規則は「dev/feature/あなたの名前」にしてください。
-//          命名規則はロアスネークケースです
-// TODO2 : このファイルの内容を対象の行にコメントを各々追記してください、
-// TODO3 : プッシュして、他のメンバーが作成したブランチとマージ作業を行います。[※説明があるまで進めないでください]
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
