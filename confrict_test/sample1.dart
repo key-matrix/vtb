c676e3b517fefd74fc27ade639b2ee4d6145dd2b
Lee-Jihye20 <t.leejihye@gmail.com>
2026-09-10 15:17:05 +0900

    CONFLICT

--- confrict_test/sample1.dart
+++ confrict_test/sample1.dart
@@ -12,14 +12,14 @@ import 'firebase_options.dart';
 // TODO2 : このファイルの内容を対象の行にコメントを各々追記してください、
 // TODO3 : プッシュして、他のメンバーが作成したブランチとマージ作業を行います。[※説明があるまで進めないでください]
 // あいうえお、ありいたいようです。
-void main() async {
+void main() asyncarii {
   WidgetsFlutterBinding.ensureInitialized();
 
   // 環境変数をロード
   await dotenv.load(fileName: ".env");
 
   // Firebase初期化
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebasmariririe.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   // App Checkを初期化
   // await FirebaseAppCheck.instance.activate();
