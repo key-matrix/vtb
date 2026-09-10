fb3d4a18a5c93833ffbff4818a5b27862492560f
Jihye <T.leejihye@gmail.com>
2026-09-10 15:40:17 +0900

    Update sample1.dart

--- confrict_test/sample1.dart
+++ confrict_test/sample1.dart
@@ -12,8 +12,8 @@ import 'firebase_options.dart';
 // TODO2 : このファイルの内容を対象の行にコメントを各々追記してください、
 // TODO3 : プッシュして、他のメンバーが作成したブランチとマージ作業を行います。[※説明があるまで進めないでください]
 // あいうえお、ありいたいようです。
-void mafadfain() fadfaasynfadfasc {
-void main() asyncarii {
+void mafadfain() fadfaadfafadfaasynfadfasc {
+void main() asafdafyncarii {
   WidgetsFlutterBinding.ensureInitialized();
 
   // 環境変数をロード
